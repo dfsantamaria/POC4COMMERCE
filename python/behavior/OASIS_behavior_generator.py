9777c6b54cb051989141f52d202c47fa3a501e61
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2021-06-12 11:46:40 +0200

    Behavior Template added

--- python/behavior/OASIS_behavior_generator.py
+++ python/behavior/OASIS_behavior_generator.py
@@ -1,7 +1,7 @@
 from rdflib import *
 
 class Behavior:
-    def __init__(self, ontologyGraph, ontologyNamespace): # INPUT: the user ontology, the user ontology namespace
+    def __init__(self, ontologyGraph, ontologyNamespace, ontologyTemplateGraph, ontologyTemplateNamespace, ): # INPUT: the user ontology, the user ontology namespace
         self.oasisURL = "https://www.dmi.unict.it/santamaria/projects/oasis/sources/rdf/oasis-rdf.owl"  # OASIS ontology URL
         self.oasisABoxURL = "https://www.dmi.unict.it/santamaria/projects/oasis/sources/rdf/oasis-abox-rdf.owl"  # OASIS-ABox ontology URL
 
@@ -10,7 +10,8 @@ class Behavior:
         #
         self.oasisOnt = self.loadOntology(self.oasisURL)  # OASIS ontology object
         self.oasisABoxOnt = self.loadOntology(self.oasisABoxURL)  # OASIS-ABox ontology object
-
+        self.owlobj = URIRef("http://www.w3.org/2002/07/owl#ObjectProperty")
+        self.owldat = URIRef("http://www.w3.org/2002/07/owl#DatatypeProperty")
         self.oasisNamespace=self.getNamespace(self.oasisOnt)  # OASIS ontology namespace
         self.oasisABoxNamespace=self.getNamespace(self.oasisABoxOnt)  # OASIS-ABox ontology namespace
 
@@ -23,6 +24,15 @@ class Behavior:
         else:
            self.baseNamespace = ontologyNamespace
         self.baseAgent = None  # User agent name
+
+        #
+        #user agent template ontology data
+        #
+        self.baseTemplateOntology = ontologyTemplateGraph  # User template ontology
+        if ontologyTemplateNamespace == None:  # Computing user template ontology namespace
+            self.baseTemplateNamespace = self.getNamespace(self.baseTemplateOntology)
+        else:
+            self.baseTemplateNamespace = ontologyTemplateNamespace
 
         return
 
@@ -46,21 +56,114 @@ class Behavior:
     def getOASISEntityByName(self, name):
         return self.oasisNamespace + name
 
-    # Create an user agent given the agent entity name
-    def createAgent(self, agentName):
-        self.baseAgent = self.baseNamespace + agentName
-        self.baseOntology.add((URIRef(self.baseAgent), RDF.type, self.getOASISEntityByName("Agent")))
-        #print(self.baseNamespace, self.oasisNamespace, self.oasisABoxNamespace)
-        return self.baseAgent
+    # Get the IRI of OASIS-Abox entities given their entity name
+    def getOASISABoxEntityByName(self, name):
+        return self.oasisABoxNamespace + name
+
+    def addObjPropAssertion(self, ontology, subject, property, object):
+        ontology.add((URIRef(subject), URIRef(property), URIRef(object)))
+
+    def addClassAssertion(self, ontology, instance, owlclass):
+        ontology.add((URIRef(instance), RDF.type, URIRef(owlclass)))
+
+    def addOWLObjectProperty(self, ontology, property):
+        self.addClassAssertion(ontology, property, self.owlobj)
+
+    def addOWLDataProperty(self, ontology, property):
+        self.addClassAssertion(ontology, property, self.owldat)
 
     # Add to ontology with the selected namespace an owl:imports axiom for each passed IRI.
     #INPUT the ontology and the ontology IRI that will include the owl:imports axiom, a list of IRI to be included in the ontology
     def addImportAxioms(self, ontology, ontologyNS, namespaceToImport):
         for s in namespaceToImport:
-            ontology.add((URIRef(ontologyNS), OWL.imports, URIRef(s)))
+            self.addObjPropAssertion(ontology, URIRef(ontologyNS), OWL.imports, URIRef(s))
+
+
+    def getNoAnchorNamespace(self, namespace):
+        if namespace.endswith('#'):
+           return namespace[:-1]
+        return namespace
 
     #import OASIS and OASIS-Abox in the current ontology
-    def addImportOASIS(self):
-        self.addImportAxioms(self.baseOntology, self.baseNamespace, [self.oasisNamespace, self.oasisABoxNamespace])
+    def addImportOASIS(self, ontology, namespace):
+        self.addImportAxioms(ontology, namespace, [ self.getNoAnchorNamespace(self.oasisNamespace), self.getNoAnchorNamespace(self.oasisABoxNamespace)])
+        ontology.bind("oasis", self.oasisNamespace)
+        ontology.bind("oabox", self.oasisABoxNamespace)
+
+        # Create an user agent given the agent entity name
+
+    def createAgent(self, agentName):
+        self.baseAgent = self.baseNamespace + agentName
+        self.addClassAssertion(self.baseOntology, self.baseAgent, self.getOASISEntityByName("Agent"))
+        return self.baseAgent
+
+    #create an OASIS agent template given its name
+    def createAgentTemplate(self, agentName):
+        baseTemplateAgent = self.baseTemplateNamespace + agentName
+        self.addClassAssertion(self.baseTemplateOntology, baseTemplateAgent, self.getOASISEntityByName("AgentBehaviorTemplate"))
+        # print(self.baseNamespace, self.oasisNamespace, self.oasisABoxNamespace)
+        return baseTemplateAgent
 
 
+    #connect a agent template with a behavior template
+    def connectAgentToBehaviorTemplate(self, agentName, behaviorName):
+        self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName("hasBehavior"))
+        self.addObjPropAssertion(self.baseTemplateOntology, self.baseTemplateNamespace+agentName,
+                                 self.getOASISEntityByName("hasBehavior"), self.baseTemplateNamespace+behaviorName)
+      #  self.baseTemplateNamespace + behaviorName
+      #  baseTemplateAgent = self.baseTemplateNamespace + agentName
+
+    #create a behavior template given an agent template IRI
+    def createBehaviorTemplate(self, behaviorName, goalName, taskName, operators, operatorsArguments, inputs, outputs):
+        #create  and add the behavior
+        behavior = self.baseTemplateNamespace+behaviorName
+        self.addClassAssertion(self.baseTemplateOntology, behavior, self.getOASISEntityByName("Behavior"))
+
+        #create, add, and connect the goal
+        goal = self.baseTemplateNamespace+goalName
+        self.addClassAssertion(self.baseTemplateOntology, goal, self.getOASISEntityByName("GoalDescription"))
+        self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName("consistsOfGoalDescription"))
+        self.addObjPropAssertion(self.baseTemplateOntology, behavior, self.getOASISEntityByName("consistsOfGoalDescription"), goal)
+
+        # create, add, and connect the task
+        task = self.baseTemplateNamespace + taskName
+        self.addClassAssertion(self.baseTemplateOntology,task, self.getOASISEntityByName("TaskDescription"))
+        self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName("consistsOfTaskDescription"))
+        self.addObjPropAssertion(self.baseTemplateOntology, goal, self.getOASISEntityByName("consistsOfTaskDescription"), task)
+
+        #create, add, and connect the task operator
+        taskOperator = self.baseTemplateNamespace + operators[0]
+        self.addClassAssertion(self.baseTemplateOntology, taskOperator, self.getOASISEntityByName("TaskOperator"))
+        self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName("hasTaskOperator"))
+        self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName("refersExactlyTo")) #the action property
+        self.addObjPropAssertion(self.baseTemplateOntology, task, self.getOASISEntityByName("hasTaskOperator"), taskOperator)
+        self.addObjPropAssertion(self.baseTemplateOntology, taskOperator, self.getOASISEntityByName("refersExactlyTo"), self.getOASISABoxEntityByName(operators[1])) #the action
+
+        # create, add, and connect the task operator argument
+        if operatorsArguments:
+           taskOperatorArgument = self.baseTemplateNamespace + operatorsArguments[0]
+           self.addClassAssertion(self.baseTemplateOntology, taskOperatorArgument, self.getOASISEntityByName("TaskOperatorArgument"))
+           self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName("hasTaskOperatorArgument"))
+           self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName("refersExactlyTo"))  # the action property
+           self.addObjPropAssertion(self.baseTemplateOntology, task, self.getOASISEntityByName("hasTaskOperatorArgument"), taskOperatorArgument)
+           self.addObjPropAssertion(self.baseTemplateOntology, taskOperatorArgument, self.getOASISEntityByName("refersExactlyTo"), self.getOASISABoxEntityByName(operatorsArguments[1]))  # the action
+
+        # create, add, and connect the task input parameters
+        if inputs:
+            for input in inputs:
+                inputName = self.baseTemplateNamespace + input[0]
+                self.addClassAssertion(self.baseTemplateOntology, inputName, self.getOASISEntityByName("TaskInputParameterTemplate"))
+                self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName("hasTaskInputParameterTemplate"))
+                self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName(input[1]))  # the input property
+                self.addObjPropAssertion(self.baseTemplateOntology, task, self.getOASISEntityByName("hasTaskInputParameterTemplate"), inputName)
+                self.addObjPropAssertion(self.baseTemplateOntology, inputName, self.getOASISEntityByName(input[1]),  input[2])  # the action
+
+        # create, add, and connect the task input parameters
+        if outputs:
+           for output in outputs:
+               outputName = self.baseTemplateNamespace + output[0]
+               self.addClassAssertion(self.baseTemplateOntology, outputName, self.getOASISEntityByName("TaskOutputParameterTemplate"))
+               self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName("hasTaskOutputParameterTemplate"))
+               self.addOWLObjectProperty(self.baseTemplateOntology, self.getOASISEntityByName(output[1]))  # the input property
+               self.addObjPropAssertion(self.baseTemplateOntology, task, self.getOASISEntityByName("hasTaskOutputParameterTemplate"), outputName)
+               self.addObjPropAssertion(self.baseTemplateOntology, outputName, self.getOASISEntityByName(output[1]), output[2])  # the action
--- python/behavior/test/test_behavior_gen.py
+++ python/behavior/test/test_behavior_gen.py
@@ -1,15 +1,56 @@
 from OASIS_behavior_generator import *
 
 #test
-#create a fresh ontology
+#create a fresh ontology for the agent
 namespace =  Namespace("http://www.ontochain.org/myOntology#")
 ontology=Graph()
-ontology.bind("xml:base", namespace)
+ontology.bind("base", namespace)
 ontology.bind("owl","http://www.w3.org/2002/07/owl#")
+
+#create a fresh ontology for the agent template
+namespaceTemp =  Namespace("http://www.ontochain.org/myOntologyTemplate#")
+ontologyTemp=Graph()
+ontologyTemp.bind("base", namespaceTemp)
+ontologyTemp.bind("owl","http://www.w3.org/2002/07/owl#")
+
 # Create the graph
-b = Behavior(ontology, namespace)
+b = Behavior(ontology, namespace, ontologyTemp, namespaceTemp)
+
+
+
 #import OASIS into the current ontology
-b.addImportOASIS()
+b.addImportOASIS(ontology, namespace)
+b.addImportOASIS(ontologyTemp, namespaceTemp)
+
+
+#create agent template
+agentTemplateName=b.createAgentTemplate("MyAgentBehaviorTemplate")
+#create behavior template
+input1 = "http://www.ontochain.org/myOntologyTemplate#input-entity-1"
+output1 = "http://www.ontochain.org/myOntologyTemplate#output-entity-1"
+b.createBehaviorTemplate("MyBehavior", "MyGoal", "MyTask",
+                         ["MyTaskOperator", "turn"],
+                         ["MyOperatorArgument", "off"],
+                         [
+                             ["MyInput1", "refersAsNewTo", input1]
+                         ],
+                         [
+                             ["MyOutput1", "refersAsNewTo", output1]
+                         ])
+#connect agent to agent behavior
+b.connectAgentToBehaviorTemplate("MyAgentBehaviorTemplate", "MyBehavior")
+
+
 #Crate agent
 b.createAgent("MyAgent")
 print(ontology.serialize(format="turtle").decode("utf-8"))
+print("######################################################")
+print(ontologyTemp.serialize(format="turtle").decode("utf-8"))
+
+
+#save
+file = open("agent.owl", "w")
+file.write(ontology.serialize(format='xml').decode())
+
+file = open("agentTemplate.owl", "w")
+file.write(ontologyTemp.serialize(format='xml').decode())
