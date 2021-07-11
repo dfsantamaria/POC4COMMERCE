3886b186ef0fb5e24dfbf34addee6432b4ef0521
Daniele Francesco Santamaria <santamaria@dmi.unict.it>
2021-07-11 10:41:27 +0200

    test

--- python/behavior/test/meeting-130721/meeting-130721.py
+++ python/behavior/test/meeting-130721/meeting-130721.py
@@ -102,9 +102,11 @@ myPaypalMTObject= namespace + "paypalResource"
 myPaypalMTOutput = namespace + "PaypalReceipt"
 myPaypalMTInput2= namespace + "PaypalMTAccountSource"
 myPaypalMTInput3= namespace + "PaypalMTAccountDestination"
+myPaypalMTInput4= namespace + "PaypalMTAssetToPayFor"
 b.addClassAssertion(ontology, myPaypalMTObject, b.getOASISEntityByName("FIATCurrency"))
 b.addClassAssertion(ontology, myPaypalMTInput2, namespace+"PaypalAccount")
 b.addClassAssertion(ontology, myPaypalMTInput3, namespace+"PaypalAccount")
+b.addClassAssertion(ontology, myPaypalMTInput4, b.getOASISEntityByName("Asset"))
 b.createAgentBehavior("PaypalMTBehavior", "PaypalMTGoal", "PaypalMTTask",
                          ["PaypalMTOperator", "transfer"],
                          [],
@@ -114,7 +116,8 @@ b.createAgentBehavior("PaypalMTBehavior", "PaypalMTGoal", "PaypalMTTask",
                          [
                              ["PaypalMTInput1", "refersAsNewTo", myPaypalMTObject],
                              ["PaypalMTInput2", "refersAsNewTo", myPaypalMTInput2],
-                             ["PaypalMTInput3", "refersAsNewTo", myPaypalMTInput3]
+                             ["PaypalMTInput3", "refersAsNewTo", myPaypalMTInput3],
+                             ["PaypalMTInput4", "refersAsNewTo", myPaypalMTInput4]
 
                          ],
                          [
@@ -208,8 +211,94 @@ b.createAgentAction("AppleProducer", "appleOfferingCreation", "appleOfferingCrea
                           ]
                          ])
 
+file = open("meeting-130721.owl", "w")
+file.write(ontology.serialize(format='xml').decode())
+
+input("Press a key for the second phase")
+
+#accepting the offering
+b.createAgentAction("Bob", "acceptOffAction", "acceptOffGoal", "acceptOffTask",
+                         ["acceptOffOperator", "accept"],
+                         [],
+                         [
+                             ["acceptOffObject", "refersExactlyTo", appleOffering]
+                         ],
+                         [
+                         ],
+                         [
+                         ],
+                         [
+                          "bobOfferingTask",
+                          [
+                            ["acceptOffObject", "bobOfferingTaskObject"]
+                          ],
+                          [],
+                          []
+                    ])
+
+b.addClassAssertion(ontology, appleOffering, occommerce+"AcceptedOffering")
+
+#paying for the object
+payTransferringOperation = namespace +"goodOfferingMoneyTransfer"
+bobPaypalAccount = namespace +"bobPaypal"
+producerPaypalAccount = namespace + "producerPaypal"
+b.addDataPropAssertion(ontology, payTransferringOperation, occommerce+"hasCurrencyValue", "1000", XSD.float)
+b.addClassAssertion(ontology, namespace+"payGoodOfferingInput2", occommerce+"PaymentSource")
+b.addClassAssertion(ontology, namespace+"payGoodOfferingInput3", occommerce+"PaymentDestination")
+b.addClassAssertion(ontology, namespace+"payGoodOfferingInput4", occommerce+"PayedObject")
+b.createAgentAction("PaypalMoneyTransfer", "payGoodOfferingAction", "payGoodOfferingGoal", "payGoodOfferingTask",
+                         ["payGoodOfferingOperator", "transfer"],
+                         [],
+                         [
+                             ["payGoodOfferingObject", "refersExactlyTo", payTransferringOperation]
+                         ],
+                         [
+                            ["payGoodOfferingInput1", "refersExactlyTo", payTransferringOperation],
+                            ["payGoodOfferingInput2", "refersExactlyTo", bobPaypalAccount],
+                            ["payGoodOfferingInput3", "refersExactlyTo", producerPaypalAccount],
+                            ["payGoodOfferingInput4", "refersExactlyTo", appleBatch]
+                         ],
+                         [
+                             ["payGoodOfferingOutput1", "refersExactlyTo", payTransferringOperation]
+                         ],
+                         [
+                          "PaypalMTTask",
+                          [
+                            ["payGoodOfferingObject", "PaypalMTObject"]
+                          ],
+                          [
+                              ["payGoodOfferingInput1", "PaypalMTInput1"],
+                              ["payGoodOfferingInput2", "PaypalMTInput2"],
+                              ["payGoodOfferingInput3", "PaypalMTInput3"],
+                              ["payGoodOfferingInput4", "PaypalMTInput4"]
+                          ],
+                          [
+                              ["payGoodOfferingOutput1", "PaypalMTOutput1"]
+                          ]
+                         ])
 
 
+shipGoodOfferingReceipt=namespace+"shipGoodOfferingReceipt-track"
+b.createAgentAction("FedexCourier", "shipGoodOfferingAction", "shipGoodOfferingGoal", "shipGoodOfferingTask",
+                         ["shipGoodOfferingOperator", "ship"],
+                         [],
+                         [
+                            ["shipGoodOfferingObject", "refersExactlyTo", appleBatch]
+                         ],
+                         [
+                             ["shipGoodOfferingInput1", "refersExactlyTo", appleBatch]
+                         ],
+                         [
+                             ["shipGoodOfferingOutput", "refersExactlyTo", shipGoodOfferingReceipt]
+                         ],
+                         [
+                          "FedexShipTask",
+                          [
+                            ["shipGoodOfferingObject", "FedexShipObject"]
+                          ],
+                          ["shipGoodOfferingInput1", "FedexShipInput"],
+                          ["shipGoodOfferingOutput","FedexShipOutput1"]
+                    ])
 
 
 file = open("meeting-130721.owl", "w")
